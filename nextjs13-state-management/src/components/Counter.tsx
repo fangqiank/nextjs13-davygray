2bd328fa3e0e3b2cbe3c09fea818857a2cacff58
fangqiank <fangqiank@msn.com>
2023-06-05 15:45:27 +0800

    add react-bootstrap

--- nextjs13-state-management/src/components/Counter.tsx
+++ nextjs13-state-management/src/components/Counter.tsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux"
 import {RootState, AppDispatch} from '@/store'
 import {increment, decrement, incrementByAmount} from '../store/counterSlice'
 import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Badge from 'react-bootstrap/Badge'
 
 export const Counter = () => {
 	const dispatch = useDispatch<AppDispatch>()
@@ -14,27 +16,32 @@ export const Counter = () => {
 
 	return (
 		<div>
-			<span>{count}</span>
-			<Button
-				variant="outline-primary"
-				onClick={() => dispatch(increment())}
-			>
-				Increment
-			</Button>
-	
-			<Button
-				variant="outline-primary"
-				onClick={() => dispatch(decrement())}
-			>
-				Decrement
-			</Button>
+			<Badge bg="secondary">{count}</Badge>
+			<ButtonGroup aria-label="Basic example">
+				<Button
+					variant="outline-primary"
+					size="sm"
+					onClick={() => dispatch(increment())}
+				>
+					Increment
+				</Button>
+		
+				<Button
+					variant="outline-secondary"
+					size="sm"
+					onClick={() => dispatch(decrement())}
+				>
+					Decrement
+				</Button>
 
-			<Button
-				variant="outline-primary"
-				onClick={() => dispatch(incrementByAmount(2))}
-			>
-				Increment 2
-			</Button>
+				<Button
+					variant="outline-danger"
+					size="sm"
+					onClick={() => dispatch(incrementByAmount(2))}
+				>
+					Increment by 2
+				</Button>
+			</ButtonGroup>
 		</div>
 	)
 }
--- nextjs13-state-management/src/components/UserList.tsx
+++ nextjs13-state-management/src/components/UserList.tsx
@@ -4,6 +4,10 @@ import { RootState, AppDispatch } from "@/store"
 import { fetchUsers, increment } from "@/store/userSlice"
 import { useEffect, useRef } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 
 export const UserList = () => {
 	const {entities, loading, value} = useSelector((state: RootState) => state.user)
@@ -25,13 +29,28 @@ export const UserList = () => {
 			{loading ? (
 				<h1>Loading...</h1>
 			) : (
-				entities?.map((user: any) => <div key={user.id}>{user.name}</div> )
+				entities?.map((user: any) => 
+				<ListGroup key={user.id} horizontal>
+					<ListGroup.Item>{user.name}</ListGroup.Item>
+				</ListGroup> )
 			)}
 
-			<button onClick={() => dispatch(increment(5))}>
-				call me
-			</button>
-			<span>{value}</span>
+		<Card style={{ width: '18rem' }}>
+			<Card.Img variant="top" src="holder.js/100px180" />
+			<Card.Body>
+				<Card.Title>Test</Card.Title>
+				<Card.Text>
+					{value}
+				</Card.Text>
+				<Button 
+					variant="primary" 
+					onClick={() => dispatch(increment(5))}
+				>
+					Click on me
+				</Button>
+			</Card.Body>
+		</Card>
+			
 		</>
 	)
 }
