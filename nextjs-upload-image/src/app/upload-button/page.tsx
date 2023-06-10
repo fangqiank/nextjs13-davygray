a443978c86dcb30fd49c07796117f44c17f1c3cf
fangqiank <fangqiank@msn.com>
2023-06-10 15:14:46 +0800

    Add upload file

---
+++ nextjs-upload-image/src/app/upload-button/page.tsx
@@ -0,0 +1,66 @@
+"use client"
+ 
+// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+import "@uploadthing/react/styles.css"
+import { UploadButton } from "@uploadthing/react"
+import { OurFileRouter } from "../api/uploadthing/core"
+import {useState} from 'react'
+import Link from "next/link"
+
+export default function UploadButtonPage() {
+  const [images, setImages] = useState<{
+    fileUrl: string,
+    fileKey: string
+	}[]>([])
+
+	const title = images.length ? (
+		<>
+			<p>Upload Complete</p>
+			<p className="mt-2">{images.length} files</p>
+		</>
+	) : null
+
+	const imageList  = (
+		<>
+			{title}
+			<ul>
+				{images.map((img, idx) => (
+					<li 
+						className="mt-2"
+						key = {idx}
+					>
+						<Link 
+							href={img.fileUrl}
+							target="_blank"
+						>
+							{img.fileUrl}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+
+	return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton<OurFileRouter>
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          if(res)
+						// Do something with the response
+						setImages(res)
+						const json = JSON.stringify(res)
+						console.log(json)
+					// Do something with the response
+          // alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+
+			{imageList}
+    </main>
+  );
+}
